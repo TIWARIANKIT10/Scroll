bb3346d35a2a7bd1b5a6cde92c05759c80c3ee01
TIWARIANKIT10 <00tiwariankit00@gmail.com>
2025-03-11 14:06:26 +0545

    css added

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const date = document.getElementById('date');
  let year = new Date().getFullYear();
  date.innerHTML = year;
+
+ // close links 
+ const navToggle = document.querySelector('.nav-toggle');
+ const linkCon = document.querySelector('.links-container');
+ const link = document.querySelector('.links');
+
+
+ navToggle.addEventListener("click",()=>{
+    linkCon.classList.toggle("show-links");
+ })
